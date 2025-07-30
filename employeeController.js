46686370dc270245e60f1571b6b17bc7d9a27b74
Vishnu98123 <gugulothvishnu98@gmail.com>
2025-07-30 23:16:00 +0530

    Delete employeeController.js

--- employeeController.js
+++
@@ -1,7 +0,0 @@
-
-// Controller for employee operations
-const Employee = require('../models/Employee');
-exports.getAllEmployees = async (req, res) => {
-  const employees = await Employee.find();
-  res.json(employees);
-};
