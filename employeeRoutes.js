3a98f07046d402d8b172121aa4edd6cb6c6a99d8
Vishnu98123 <gugulothvishnu98@gmail.com>
2025-07-30 23:16:15 +0530

    Delete employeeRoutes.js

--- employeeRoutes.js
+++
@@ -1,6 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/employeeController');
-router.get('/', controller.getAllEmployees);
-module.exports = router;
