3f61e13ce095c7206dabe4bebd2331f242f810b3
Vishnu98123 <gugulothvishnu98@gmail.com>
2025-07-30 23:15:01 +0530

    Delete Employee.js

--- Employee.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-const employeeSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  position: String,
-  department: String,
-});
-module.exports = mongoose.model('Employee', employeeSchema);
